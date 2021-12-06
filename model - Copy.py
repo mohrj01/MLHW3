13870eefe69edc61fb7099d25630a8f33462a4f6
mohrj01 <jessrmohr@gmail.com>
2021-12-05 18:03:15 -0600

    hi

---
+++ model - Copy.py
@@ -0,0 +1,86 @@
+
+
+import numpy as np
+import pandas as pd
+import spacy
+from spacy.lang.en.stop_words import STOP_WORDS
+from string import punctuation
+from collections import Counter
+from heapq import nlargest
+import os
+import spacy
+nlp = spacy.load("en_core_web_sm")
+from spacy import displacy
+stopwords=list(STOP_WORDS)
+from string import punctuation
+import re
+from tqdm import tqdm
+from sentence_transformers import SentenceTransformer, util
+#!pip install sentence-transformers
+
+
+
+punctuation=punctuation+ '\n'
+
+
+df = pd.read_csv('https://raw.githubusercontent.com/mohrj01/MLHW3/master/hotelReviewsInPrague.csv')
+
+
+text = """Looking for a hotel in Prague near the bridge with free breakfast"""
+doc = nlp(text)
+sentence_spans = list(doc.sents)
+displacy.render(doc, jupyter = True, style="ent")
+
+
+embedder = SentenceTransformer('all-MiniLM-L6-v2')
+
+df['hotelName'].value_counts()
+
+df_combined = df.sort_values(['hotelName']).groupby('hotelName', sort=False).review_body.apply(''.join).reset_index(name='all_review')
+
+df_combined
+
+
+
+df_combined['all_review'] = df_combined['all_review'].apply(lambda x: re.sub('[^a-zA-z0-9\s]','',x))
+
+def lower_case(input_str):
+    input_str = input_str.lower()
+    return input_str
+
+df_combined['all_review']= df_combined['all_review'].apply(lambda x: lower_case(x))
+
+
+
+df = df_combined
+df_sentences = df_combined.set_index("all_review")
+df_sentences.head()
+
+
+
+df_sentences = df_sentences["hotel"].to_dict()
+df_sentences_list = list(df_sentences.keys())
+len(df_sentences_list)
+
+
+
+list(df_sentences.keys())[:5]
+
+
+#%%
+
+df_sentences_list = [str(d) for d in tqdm(df_sentences_list)]
+
+#%%
+
+# Corpus with example sentences
+corpus = df_sentences_list
+corpus_embeddings = embedder.encode(corpus,show_progress_bar=True)
+
+
+with open("corpus.pkl" , "wb") as file1:
+  pkl.dump(corpus,file1)
+with open("corpus_embeddings.pkl" , "wb") as file2:
+  pkl.dump(corpus_embeddings,file2)
+with open("df.pkl" , "wb") as file3:
+  pkl.dump(df,file3)
