48f7728cf6845fa58c834710c75e749ccb0b4fb0
mohrj01 <93602315+mohrj01@users.noreply.github.com>
2021-12-05 21:24:01 -0600

    Update app.py

--- app.py
+++ app.py
@@ -25,6 +25,96 @@ with open('corpus.pkl', 'rb') as file1:
     corpus = pkl.load(file1)
  
 
+model = SentenceTransformer('all-MiniLM-L6-v2')
+paraphrases = util.paraphrase_mining(model, corpus)
+#query_embeddings_p =  util.paraphrase_mining(model, queries,show_progress_bar=True)
+
+#%%
+
+queries = ['Hotel closest to bridge',
+           'hotel closest to water'
+           ]
+query_embeddings = embedder.encode(queries,show_progress_bar=True)
+
+#%%
+
+# Find the closest 5 sentences of the corpus for each query sentence based on cosine similarity
+closest_n = 5
+print("\nTop 5 most similar sentences in corpus:")
+for query, query_embedding in zip(queries, query_embeddings):
+    distances = scipy.spatial.distance.cdist([query_embedding], corpus_embeddings, "cosine")[0]
+
+    results = zip(range(len(distances)), distances)
+    results = sorted(results, key=lambda x: x[1])
+
+    print("\n\n=========================================================")
+    print("==========================Query==============================")
+    print("===",query,"=====")
+    print("=========================================================")
+
+
+    for idx, distance in results[0:closest_n]:
+        print("Score:   ", "(Score: %.4f)" % (1-distance) , "\n" )
+        print("Paragraph:   ", corpus[idx].strip(), "\n" )
+        row_dict = df.loc[df['all_review']== corpus[idx]]
+        print("paper_id:  " , row_dict['hotelName'] , "\n")
+        # print("Title:  " , row_dict["title"][corpus[idx]] , "\n")
+        # print("Abstract:  " , row_dict["abstract"][corpus[idx]] , "\n")
+        # print("Abstract_Summary:  " , row_dict["abstract_summary"][corpus[idx]] , "\n")
+        print("-------------------------------------------")
+
+#%%
+
+from sentence_transformers import SentenceTransformer, util
+import torch
+embedder = SentenceTransformer('all-MiniLM-L6-v2')
+
+corpus_embeddings = embedder.encode(corpus, convert_to_tensor=True)
+
+#%%
+
+# Query sentences:
+queries = ['Hotel closest to bridge',
+           'walking distance to water'
+           ]
+
+
+# Find the closest 5 sentences of the corpus for each query sentence based on cosine similarity
+top_k = min(5, len(corpus))
+for query in queries:
+    query_embedding = embedder.encode(query, convert_to_tensor=True)
+
+    # We use cosine-similarity and torch.topk to find the highest 5 scores
+    cos_scores = util.pytorch_cos_sim(query_embedding, corpus_embeddings)[0]
+    top_results = torch.topk(cos_scores, k=top_k)
+
+    print("\n\n======================\n\n")
+    print("Query:", query)
+    print("\nTop 5 most similar sentences in corpus:")
+
+    for score, idx in zip(top_results[0], top_results[1]):
+        print("(Score: {:.4f})".format(score))
+        print(corpus[idx], "(Score: {:.4f})".format(score))
+        row_dict = df.loc[df['all_review']== corpus[idx]]
+        print("paper_id:  " , row_dict['hotelName'] , "\n")
+    # for idx, distance in results[0:closest_n]:
+    #     print("Score:   ", "(Score: %.4f)" % (1-distance) , "\n" )
+    #     print("Paragraph:   ", corpus[idx].strip(), "\n" )
+    #     row_dict = df.loc[df['all_review']== corpus[idx]]
+    #     print("paper_id:  " , row_dict['Hotel'] , "\n")
+    """
+    # Alternatively, we can also use util.semantic_search to perform cosine similarty + topk
+    hits = util.semantic_search(query_embedding, corpus_embeddings, top_k=5)
+    hits = hits[0]      #Get the hits for the first query
+    for hit in hits:
+        print(corpus[hit['corpus_id']], "(Score: {:.4f})".format(hit['score']))
+    """
+
+
+#%%
+
+model = SentenceTransformer('sentence-transformers/paraphrase-xlm-r-multilingual-v1')
+embeddings = model.encode(corpus)
 #embedder = SentenceTransformer('all-MiniLM-L6-v2')
 #model = SentenceTransformer('all-MiniLM-L6-v2')
 #corpus_embeddings = embedder.encode(corpus, convert_to_tensor=True)
