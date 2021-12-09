2b4cecbe53a71ed010e22bf7f7c5a00d7674c7e6
mohrj01 <93602315+mohrj01@users.noreply.github.com>
2021-12-09 13:29:10 -0600

    Update app.py

--- app.py
+++ app.py
@@ -129,7 +129,8 @@ for query in queries:
             l.append(i)
         #l[0] = l[0].replace("Name: hotel_name, dtype: object", "")
         st.write("Hotel Name: ", l[0])
-        st.write("Price Per Night: ", row_dict['price_per_night'][0])
+        st.write(row_dict)
+        #st.write("Price Per Night: ", row_dict['price_per_night'][0])
         st.write("\n\n======================\n\n")
 
 
