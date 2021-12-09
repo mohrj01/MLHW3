bf1669effddf32a7a00c1a82688b131359137303
mohrj01 <93602315+mohrj01@users.noreply.github.com>
2021-12-09 11:32:32 -0600

    Update app.py

--- app.py
+++ app.py
@@ -111,7 +111,7 @@ for query in queries:
         for i in row_dict['hotelName']:
             l.append(i)
         st.write(l[0])
-        l[0] = l[0].replace(" Name: hotel_name, dtype: object", "")
+        l[0] = l[0].replace("hotel", "")
         st.write(l[0])
 
 
