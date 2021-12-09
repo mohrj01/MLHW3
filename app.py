b8454c02ac2957fc5dd5e1e4e46c0f0908bfcfe7
mohrj01 <93602315+mohrj01@users.noreply.github.com>
2021-12-09 14:04:50 -0600

    Update app.py

--- app.py
+++ app.py
@@ -5,19 +5,14 @@ import pandas as pd
 import spacy
 from spacy.lang.en.stop_words import STOP_WORDS
 from string import punctuation
-#from collections import Counter
-#from heapq import nlargest
-#import os
 nlp = spacy.load("en_core_web_sm")
-#from spacy import displacy
 import re
-#from tqdm import tqdm
 from sentence_transformers import SentenceTransformer, util
-#import scipy
 import torch
 from wordcloud import WordCloud, STOPWORDS
 import matplotlib.pyplot as plt
 
+# pickle imports
 with open("df.pkl" , "rb") as file3:
     df = pkl.load(file3)
 with open('corpus_embeddings.pkl', 'rb') as file2:
@@ -25,13 +20,12 @@ with open('corpus_embeddings.pkl', 'rb') as file2:
 with open('corpus.pkl', 'rb') as file1:
     corpus = pkl.load(file1)
  
-
+# add other dataset and merge
 df2 = pd.read_csv('https://raw.githubusercontent.com/mohrj01/MLHW3/master/HotelListInPrague.csv')
 df['hotelName'] = df['hotelName'].str.replace('\d+', '')
 def myreplace(s):
     for ch in ['Name: hotel_name, dtype: object']:
         s = s.replace(ch, '')
-
     # remove extra spaces
     s = re.sub(' +', ' ', s)
     s = s.rstrip().lstrip()
@@ -39,19 +33,17 @@ def myreplace(s):
 
 df['hotelName'] = df['hotelName'].map(myreplace)
 df2['hotelName'] = df2['hotel_name']
-
 df = pd.merge(df,df2)
 
+# fill nas for price
 df['price_per_night'] = df['price_per_night'].fillna("No Price Available")
-
 
 # Embeddings
 embedder = SentenceTransformer('all-MiniLM-L6-v2')
 model = SentenceTransformer('all-MiniLM-L6-v2')
 paraphrases = util.paraphrase_mining(model, corpus)
-#corpus_embeddings = embedder.encode(corpus, convert_to_tensor=True)
 
-
+# Hotel FInder
 st.header("Prague Hotel Finder")
 st.write("Jessica Mohr | MABA ML HW3 | [Github](%s)" % "https://github.com/mohrj01/MLHW3")
 user_input = st.text_input("What type of hotel are you searching for?", value="")
@@ -61,13 +53,14 @@ query_embeddings = embedder.encode(user_input,show_progress_bar=True)
 
 # add other useless words to stop words list
 stop_words=list(STOP_WORDS)+['hotel', 'Hotel', 'Prague', 'stay']
-punctuation=punctuation+ '\n'
+#punctuation=punctuation+ '\n'
+# define word cloud
 def plot_cloud(wordcloud):
     plt.figure()
     plt.imshow(wordcloud) 
     plt.axis("off");
         
-        
+# find the top 5        
 top_k = min(5, len(corpus))
 for query in queries:
     query_embedding = embedder.encode(query, convert_to_tensor=True)
@@ -85,11 +78,10 @@ for query in queries:
         l=[]
         for i in row_dict['hotelName']:
             l.append(i)
-        #l[0] = l[0].replace("Name: hotel_name, dtype: object", "")
         st.write("Hotel Name: ", l[0])
         st.write("Price Per Night: ", row_dict['price_per_night'].values[0])
         st.write("[Link to Hotel](%s)" % row_dict['url'].values[0])
-        
+        # create word cloud
         wordcloud = WordCloud(stopwords = stop_words).generate(corpus[idx])
         fig, ax = plt.subplots()
         plt.imshow(wordcloud, interpolation='bilinear')
@@ -97,19 +89,7 @@ for query in queries:
         plt.show()
         st.pyplot(fig)
         st.set_option('deprecation.showPyplotGlobalUse', False)
-        
         st.write("\n\n======================\n\n")
-
-
-
-
-
-
-#st.write(len(df['hotelName'][0]))
-#st.write(len(df2['hotelName'][0]))
-
-#st.write("8", df['hotelName'][0], "8")
-#st.write("8", df2['hotelName'][0], "8")
 
 st.header("Note:")
 st.write("Eric and Dev both helped with debugging")
