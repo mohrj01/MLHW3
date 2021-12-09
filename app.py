9fb2ef209cd0041e7e8ce15364a71d4a7a216c4d
mohrj01 <93602315+mohrj01@users.noreply.github.com>
2021-12-09 11:35:38 -0600

    Update app.py

--- app.py
+++ app.py
@@ -106,13 +106,13 @@ for query in queries:
     for score, idx in zip(top_results[0], top_results[1]):
         st.write("(Score: {:.4f})".format(score))
         row_dict = df.loc[df['all_review']== corpus[idx]]
-        st.write("Hotel Name:  " , row_dict['hotelName'].values[0].replace("Name: hotel_name, dtype: object", ""), "\n")
         l=[]
         for i in row_dict['hotelName']:
             l.append(i)
         st.write(l[0])
         l[0] = l[0].replace("Name: hotel_name, dtype: object", "")
         st.write(l[0])
+        st.write("\n\n======================\n\n")
 
 
 
